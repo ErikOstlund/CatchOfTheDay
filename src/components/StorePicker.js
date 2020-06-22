6f032f539250ce68daf022436e1d768d1f2c5d79
Erik Ostlund <eostlund74@gmail.com>
2020-06-22 12:39:51 -0500

    Submit StorePicker Form
    Created ref. Send event to custom function and change route based on store name.

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,12 +2,26 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+    myInput = React.createRef();
+
+    goToStore = (event) => {
+        // Stop the form from submitting
+        event.preventDefault();
+
+        // get text from the input
+        const storeName = this.myInput.current.value;
+
+        // route user to /store/text-input
+        this.props.history.push(`/store/${storeName}`);
+    };
+
     render() {
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore}>
                 <h2>Please Enter A Store</h2>
                 <input
                     type="text"
+                    ref={this.myInput}
                     placeholder="Store Name"
                     defaultValue={getFunName()}
                     required
