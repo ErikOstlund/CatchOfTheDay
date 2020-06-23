1e9d2b3a866829f56beaad2b5180cd288a3edac5
Erik Ostlund <eostlund74@gmail.com>
2020-06-22 20:49:22 -0500

    Initializing and Setting State
    Create new AddFishForm component.
    Initialized app state.
    Sending method down via props.
    Updating state.

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,25 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+    state = {
+        fishes: {},
+        order: {}
+    };
+
+    addFish = (fish) => {
+        // Make a copy of the existing state
+        const updatedFishes = { ...this.state.fishes };
+
+        // Add the new fish to the copied state
+        // NOTE: using a timestamp for the fish number
+        updatedFishes[`fish${Date.now()}`] = fish;
+
+        // use React api to update the state
+        this.setState({
+            fishes: updatedFishes
+        });
+    };
+
     render() {
         return (
             <div className="catch-of-the-day">
@@ -11,7 +30,7 @@ class App extends React.Component {
                     <Header tagline="Fresh Seafood Market" />
                 </div>
                 <Order />
-                <Inventory />
+                <Inventory addFish={this.addFish} />
             </div>
         );
     }
