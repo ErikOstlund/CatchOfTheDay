30fffa746fdd82d6bb53f368b9de16754573efee
Erik Ostlund <eostlund74@gmail.com>
2020-06-25 15:25:02 -0500

    Display State in the Order Component
    Send state down via props.
    List each fish with the qty and total price for that fish.
    Display a Grand Total

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,8 +1,52 @@
 import React from 'react';
+import { formatPrice } from '../helpers';
 
 class Order extends React.Component {
+    renderOrder = (key) => {
+        const fish = this.props.fishes[key];
+        const count = this.props.order[key];
+        const isAvailable = fish.status === 'available';
+
+        if (!isAvailable) {
+            return (
+                <li key={key}>
+                    Sorry, {fish ? fish.name : 'fish'} is no longer available.
+                </li>
+            );
+        }
+
+        return (
+            <li key={key}>
+                {count} lbs {fish.name}
+                {formatPrice(count * fish.price)}
+            </li>
+        );
+    };
+
     render() {
-        return <div className="order">Order</div>;
+        const orderIDs = Object.keys(this.props.order);
+
+        const grandTotal = orderIDs.reduce((accumulator, key) => {
+            const fish = this.props.fishes[key];
+            const count = this.props.order[key];
+            const isAvailable = fish && fish.status === 'available';
+
+            if (isAvailable) {
+                return accumulator + count * fish.price;
+            }
+            return accumulator;
+        }, 0);
+
+        return (
+            <div className="order-wrap">
+                <h2>Order</h2>
+                <ul className="order">{orderIDs.map(this.renderOrder)}</ul>
+                <div className="total">
+                    Total:
+                    <strong>{formatPrice(grandTotal)}</strong>
+                </div>
+            </div>
+        );
     }
 }
 
